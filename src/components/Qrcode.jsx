bcecacd4a6bdf5862140d80ee534cec28938e17f
Timothy <tim87henry@gmail.com>
2023-06-29 18:09:17 +1000

    Completed layout and styling changes

--- src/components/Qrcode.jsx
+++ src/components/Qrcode.jsx
@@ -3,13 +3,13 @@ import image from '../images/image-qr-code.png';
 
 const Qrcode = () => {
   return (
-    <div className="flex items-center flex-col justify-between p-5 bg-white shadow-lg rounded-xl h-3/5 w-80">
+    <div className="flex items-center flex-col p-3 bg-white shadow-lg rounded-xl h-[460px] w-[300px]">
       <section className='h-3/5 w-full'>
         <img src={image} alt="test" className='h-full w-full shadow-lg rounded-xl'></img>
       </section>
-      <section>
-        <h1 className='text-xl'>Improve your front-end skills by building projects</h1>
-        <p>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
+      <section className='text-center mt-5 mx-2.5'>
+        <h1 className='font-extrabold leading-tight text-[20px]'>Improve your front-end skills by building projects</h1>
+        <p className='font-normal text-[15px] mt-3 leading-tight'>Scan the QR code to visit Frontend Mentor and take your coding skills to the next level</p>
       </section>
     </div>
   )
